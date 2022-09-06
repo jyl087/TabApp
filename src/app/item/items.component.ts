05084cc97ca0231aa735d245f628925bc758f40c
jyl087 <jyl087@gmail.com>
2022-09-05 21:33:38 -0700

    Add files via upload

---
+++ src/app/item/items.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core'
+import { ListViewEventData, RadListView, SwipeActionsEventData} from "nativescript-ui-listview";
+
+import { Item } from './item'
+import { ItemService } from './item.service'
+
+@Component({
+  selector: 'ns-items',
+  templateUrl: './items.component.html',
+})
+export class ItemsComponent implements OnInit {
+  items: Array<Item>
+
+  constructor(private itemService: ItemService) {}
+
+  ngOnInit(): void {
+    this.items = this.itemService.getItems()
+  }
+
+
+  onListLoaded( ev)
+  {
+    const lv = ev.object as RadListView;
+    console.log( "ListView loaded:",lv);
+  }
+}
